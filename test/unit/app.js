5f420b0ae91adc01683f9444bf8f7634435f1897
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-10-02 23:15:21 +0200

    test: add integration test

--- test/unit/app.js
+++ test/unit/app.js
@@ -1,5 +1,17 @@
+const { exec } = require('child_process');
+const { mkdtemp, readFile } = require('fs');
+const { tmpdir } = require('os');
+const { join } = require('path');
+const { promisify } = require('util');
+const rimraf = require('rimraf');
+
 // @todo If the EventEmitter gets patched during the test it will throw an RangeError "Maximum call stack size exceeded".
 global.__Zone_disable_EventEmitter = true; // eslint-disable-line camelcase
+
+const execAsync = promisify(exec);
+const mkdtempAsync = promisify(mkdtemp);
+const readFileAsync = promisify(readFile);
+const rimrafAsync = (path) => new Promise((resolve, reject) => rimraf(path, (err) => (err === null) ? resolve() : reject(err)));
 
 describe('angular-prerender', () => {
 
@@ -7,6 +19,30 @@ describe('angular-prerender', () => {
         expect(() => {
             require('../../build/node/app');
         }).to.throw(Error, 'This script is meant to be executed from the command line.');
+    });
+
+    it('should render the default URL', async function () {
+        this.timeout(240000);
+
+        const directory = await mkdtempAsync(tmpdir());
+
+        await execAsync('npx --package @angular/cli ng new universe --routing', { cwd: directory });
+
+        const projectDirectory = join(directory, 'universe');
+
+        await execAsync('ng generate universal --client-project universe', { cwd: projectDirectory });
+        await execAsync('ng build', { cwd: projectDirectory });
+        await execAsync('ng run universe:server', { cwd: projectDirectory });
+        await execAsync('npm i angular-prerender --save-dev', { cwd: projectDirectory });
+        await rimrafAsync(join(directory, 'build/node'));
+        await execAsync(`cp -r ${ join(__dirname, '../../build/node') } ${ join(projectDirectory, '/node_modules/angular-prerender/build') }`);
+        await execAsync('node node_modules/angular-prerender/build/node/app.js', { cwd: projectDirectory });
+
+        const content = await readFileAsync(join(projectDirectory, 'dist/universe/index.html'));
+
+        expect(content).to.match(/<h1(.*)> Welcome to universe! <\/h1>/);
+
+        await rimrafAsync(directory);
     });
 
 });
