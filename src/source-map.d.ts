f24d61fac975db9414cf382f97a3c7887c28d9a6
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-02-08 17:26:40 +0100

    fix: remove obsolete type definition

--- src/source-map.d.ts
+++
@@ -1,7 +0,0 @@
-// @todo Currently @angular-devkit/core at version 8.3.5 requires this crafted type declaration to compile.
-
-declare module 'source-map' {
-
-    export interface Position { } // tslint:disable-line:interface-name no-empty-interface
-
-}
