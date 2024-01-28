b5d893ccca503e09a10518dfbf870852fc7b908d
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-01-28 13:20:18 +0100

    test: test standalone and legacy applications

--- test/integration/app.js
+++ test/integration/app.js
@@ -20,23 +20,12 @@ const makeFakedTemporaryDirectory = async () => {
 
 describe('angular-prerender', () => {
     let angularMajorVersion;
-    let directory;
     let peerDependencies;
-    let projectDirectory;
     let version;
-
-    after(async function () {
-        this.timeout(600000);
-
-        await execAsync(`rm ${join(directory, `angular-prerender-${version}.tgz`)}`);
-
-        await rimraf(directory);
-    });
 
     before(async function () {
         this.timeout(600000);
 
-        directory = env.CI ? await makeFakedTemporaryDirectory() : await mkdtempAsync(`${tmpdir()}${sep}`);
         ({ peerDependencies, version } = JSON.parse(readFileSync(new URL('../../package.json', import.meta.url), 'utf8')));
 
         if (env.CI) {
@@ -45,65 +34,81 @@ describe('angular-prerender', () => {
         }
 
         angularMajorVersion = peerDependencies['@angular/core'].split(/\./).shift();
-
-        await execAsync(`npx --package @angular/cli@${angularMajorVersion} --call "ng new universe --no-interactive --routing"`, {
-            cwd: directory
-        });
-
-        projectDirectory = join(directory, 'universe');
-
-        await execAsync(`npx --package @angular/cli@${angularMajorVersion} --call "ng add @angular/ssr --skip-confirmation"`, {
-            cwd: projectDirectory
-        });
-        await execAsync('git add --all', { cwd: projectDirectory });
-        await execAsync('git commit --amend --no-edit --no-verify', { cwd: projectDirectory });
-
-        await execAsync('npm pack');
-        await execAsync(`mv ${join(cwd(), `angular-prerender-${version}.tgz`)} ${directory}`);
     });
 
-    beforeEach(async () => {
-        await execAsync('git checkout .', { cwd: projectDirectory });
-        await execAsync('git clean --force', { cwd: projectDirectory });
-    });
+    for (const standalone of [true, false]) {
+        describe(`with the --standalone flag set to "${standalone}"`, () => {
+            let directory;
+            let projectDirectory;
 
-    describe('without any peer dependency', () => {
-        beforeEach(async function () {
-            this.timeout(600000);
-
-            await execAsync(`npx --package @angular/cli@${angularMajorVersion} --call "ng build"`, { cwd: projectDirectory });
-        });
-
-        describe('when installed as peer dependency', () => {
-            it('should render the default URL', async function () {
+            after(async function () {
                 this.timeout(600000);
 
-                await execAsync(`npm install ${join(directory, `angular-prerender-${version}.tgz`)}`, { cwd: projectDirectory });
-                await execAsync('npx angular-prerender --target universe:build', {
-                    cwd: projectDirectory
-                });
+                await execAsync(`rm ${join(directory, `angular-prerender-${version}.tgz`)}`);
 
-                const content = await readFileAsync(join(projectDirectory, 'dist/universe/browser/index.html'), 'utf8');
-
-                expect(content).to.match(/<h1.*>Hello, universe<\/h1>/);
+                await rimraf(directory);
             });
-        });
 
-        describe('when invoked with npx', () => {
-            it('should render the default URL', async function () {
+            beforeEach(async function () {
                 this.timeout(600000);
+
+                directory = env.CI ? await makeFakedTemporaryDirectory() : await mkdtempAsync(`${tmpdir()}${sep}`);
 
                 await execAsync(
-                    `npx ${relative(projectDirectory, join(directory, `angular-prerender-${version}.tgz`))} --target universe:build`,
+                    `npx --package @angular/cli@${angularMajorVersion} --call "ng new universe --no-interactive --routing --standalone ${standalone}"`,
                     {
-                        cwd: projectDirectory
+                        cwd: directory
                     }
                 );
 
-                const content = await readFileAsync(join(projectDirectory, 'dist/universe/browser/index.html'), 'utf8');
+                projectDirectory = join(directory, 'universe');
 
-                expect(content).to.match(/<h1.*>Hello, universe<\/h1>/);
+                await execAsync(`npx --package @angular/cli@${angularMajorVersion} --call "ng add @angular/ssr --skip-confirmation"`, {
+                    cwd: projectDirectory
+                });
+                await execAsync('git add --all', { cwd: projectDirectory });
+                await execAsync('git commit --amend --no-edit --no-verify', { cwd: projectDirectory });
+
+                await execAsync('npm pack');
+                await execAsync(`mv ${join(cwd(), `angular-prerender-${version}.tgz`)} ${directory}`);
+
+                await execAsync('git checkout .', { cwd: projectDirectory });
+                await execAsync('git clean --force', { cwd: projectDirectory });
+
+                await execAsync(`npx --package @angular/cli@${angularMajorVersion} --call "ng build"`, { cwd: projectDirectory });
+            });
+
+            describe('when installed as peer dependency', () => {
+                it('should render the default URL', async function () {
+                    this.timeout(600000);
+
+                    await execAsync(`npm install ${join(directory, `angular-prerender-${version}.tgz`)}`, { cwd: projectDirectory });
+                    await execAsync('npx angular-prerender --target universe:build', {
+                        cwd: projectDirectory
+                    });
+
+                    const content = await readFileAsync(join(projectDirectory, 'dist/universe/browser/index.html'), 'utf8');
+
+                    expect(content).to.match(/<h1.*>Hello, universe<\/h1>/);
+                });
+            });
+
+            describe('when invoked with npx', () => {
+                it('should render the default URL', async function () {
+                    this.timeout(600000);
+
+                    await execAsync(
+                        `npx ${relative(projectDirectory, join(directory, `angular-prerender-${version}.tgz`))} --target universe:build`,
+                        {
+                            cwd: projectDirectory
+                        }
+                    );
+
+                    const content = await readFileAsync(join(projectDirectory, 'dist/universe/browser/index.html'), 'utf8');
+
+                    expect(content).to.match(/<h1.*>Hello, universe<\/h1>/);
+                });
             });
         });
-    });
+    }
 });
