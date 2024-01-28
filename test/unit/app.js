aaa4cde4207db3f3e1fc5d3ef91fc28390524c5c
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-01-28 14:59:29 +0100

    test: provide minimal command line args

--- test/unit/app.js
+++ test/unit/app.js
@@ -1,4 +1,14 @@
+import { argv } from 'process';
+
 describe('angular-prerender', () => {
+    afterEach(() => {
+        argv.slice(0, -2);
+    });
+
+    beforeEach(() => {
+        argv.push('--config', '/dev/null');
+    });
+
     it('should not export anything', async () => {
         const app = await import('../../build/node/app.js');
 
