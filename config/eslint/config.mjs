2e85276463c59c16affddd54ecd617f6a7f7693f
Christoph Guttandin <chrisguttandin@media-codings.com>
2026-06-09 16:25:49 +0200

    build(config): update linting configuration for ESLint v9

---
+++ config/eslint/config.mjs
@@ -0,0 +1,14 @@
+import config from 'eslint-config-holy-grail';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig([
+    {
+        extends: [config],
+        languageOptions: {
+            globals: { ...globals.node }
+        },
+        rules: { 'no-sync': 'off', 'node/no-missing-require': 'off' }
+    }
+]);
